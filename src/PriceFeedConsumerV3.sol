a15c5d23fcb055271c8f689b038e54901e58ac09
Mr.PAPI <thesnakewitcher@gmail.com>
2023-04-28 19:05:30 -0400

    ref: create _setPriceFeed internal function and use it in public functions

--- src/PriceFeedConsumerV3.sol
+++ src/PriceFeedConsumerV3.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "./IAggregatorV3.sol";
 
 contract PriveFeedConsumerV3 {
+
     address constant ETH_VS_USD_FEED_ADDRESS = 0x0715A7794a1dc8e42615F059dD6e406A6594651A ;
 
     AggregatorV3Interface internal priceFeed;
@@ -14,15 +15,14 @@ contract PriveFeedConsumerV3 {
      *         Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
      */
     constructor() {
-        // priceFeed = AggregatorV3Interface(ETH_VS_USD_FEED_ADDRESS);
-        setPriceFeed(ETH_VS_USD_FEED_ADDRESS);
+        _setPriceFeed(ETH_VS_USD_FEED_ADDRESS);
     }
 
     /**
      * @dev set `priceFeed` aggregator to fetch data from `address`
      */
     function setPriceFeed(address _address) public {
-        priceFeed = AggregatorV3Interface(_address);
+        _setPriceFeed(_address) ;
     }
 
     /**
@@ -60,6 +60,13 @@ contract PriveFeedConsumerV3 {
         ) = priceFeed.getRoundData(roundId);
         require(timeStamp > 0, "Round not complete");
         return price;
+    }
+
+    /**
+     * @dev set `priceFeed` aggregator to fetch data from `address`
+     */
+    function _setPriceFeed(address _address) internal {
+        priceFeed = AggregatorV3Interface(_address);
     }
 
 }
